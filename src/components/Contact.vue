052d4fcbc297c90501f3bdf1f87aca1c38d3184d
Yuxin Wu <876465382@qq.com>
2018-11-17 15:15:04 +0800

    所有页面基本完成 只差首页滚动条

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="contact">
-    
-  </div>
+	<div class="frame">
+		<div class="contact">
+	    	<h2>关于网站</h2>
+		    <p>网站处于建设初期，仅适合屏宽1920的设备进行浏览，稍有不便还请见谅。</p>
+		    <p>如想提交BUG可以发送邮件至biokube@qq.com，谢谢！</p>
+		    <h2>商务合作</h2>
+		    <p><b>电玩分部涉及游戏可以联系</b></p>
+		    <p>QQ：1870071075</p>
+		    <p>微信：BIOKUBE</p>
+		    <p>邮箱：biokube@qq.com</p>
+		    <p>上课时间无法及时回复还请见谅！</p>
+		    <p><b>其他游戏可以联系</b></p>
+		    <p>手机：18061790350</p>
+		    <p>微信：wayay1999</p>
+		    <p>邮箱：wayay1999@126.com</p>
+		    <p>上课时间无法及时回复还请见谅！</p>
+	  	</div>
+	</div>
+  	
 </template>
 
 <script>
@@ -17,4 +33,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h2{
+	margin-top: 40px;
+}
+p{
+	margin-top: 20px;
+}
+.frame{
+	position: relative;
+}
+.contact{
+	position: relative;
+	width: 1240px;
+	height: auto;
+	left: 50%;
+	margin-left: -620px;
+	margin-bottom: 150px;
+}
 </style>
--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,20 +1,66 @@
 <template>
-  <div class="partner">
-    
-  </div>
+	<div class="partner">
+	    <PartnerCard v-for="partner in partnerList" v-bind:PartnerInfo="partner"></PartnerCard>
+	</div>
 </template>
 
 <script>
+import PartnerCard from './utils/PartnerCard'
 export default {
   name: 'Partner',
   data () {
     return {
-      
+	    partnerList:[
+	    {
+	    	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/logo/sonkwo.png",
+	    	title:"关于杉果游戏",
+	    	content:[
+		    	"杉果游戏®是中电博亚创立的游戏品牌，以数字分销平台为基础，致力于为中国玩家提供最好的游戏作品、顶级的游戏服务，以及核心玩家专属的游戏内容社区。",
+		    	"杉果游戏®商城于2013年1月18日在中国地区正式上线。截止2017年，杉果游戏已成功发布超过2,500款作品，无论是AAA级制作的顶级作品，还是获奖无数的精致独立游戏，都在通过杉果游戏®商城呈献给中国游戏玩家。伴随游戏产业的发展，游戏商城的作品数量每天继续以飞快的速度增长，不断带给中国玩家更多的选择和体验。",
+		    	"此外，杉果游戏®社区也已正式开放，玩家可以通过PC或移动设备等多种终端访问社区，满足玩家对于游戏经验、资讯、活动等信息的分享与沟通需求。杉果游戏®社区致力于为全球以中文为基础的游戏玩家打造全方位的社交中心，同时为他们提供最顶级的游戏服务。",
+		    	]
+	    },
+	    {
+	    	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/logo/cubejoy.png",
+	    	title:"关于方块游戏",
+	    	content:[
+		    	"方块游戏为国内上市公司皖新传媒（601801）旗下全资公司，公司核心业务为PC游戏发行、代理、运营，将不断通过代理引进、自研开发、合作销售等多种方式将全球优秀作品引入中囯，并通过自有发行平台“方块游戏”为用户提供购买、下载、激活、社群等一系列游戏体验服务。"
+		    	]
+	    },
+	    {
+	    	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/logo/tencent.png",
+	    	title:"关于腾讯互娱",
+	    	content:[
+		    	"腾讯互动娱乐，全球领先的综合互动娱乐服务品牌，旗下涵盖腾讯游戏、腾讯文学、腾讯动漫等多个互动娱乐业务平台，致力为用户提供包括网络游戏、文学、动漫、戏剧、影视等在内的多元化、高品质综合互动娱乐体验。立足“泛娱乐”战略，腾讯互动娱乐持续基于互联网与移动互联网的多领域共生，打造明星IP（知识产权，Intellectual Property）的粉丝经济，全面拥抱互动娱乐新时代，让想象绽放！！"
+		    	]
+	    },
+	    {
+	    	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/logo/sng.png",
+	    	title:"关于SNG",
+	    	content:[
+		    	"苏宁电子竞技俱乐部（SNG）成立于2015年，现拥有英雄联盟、绝地求生、堡垒之夜等游戏分部。其中英雄联盟分部于2017年获得LSPL春季赛冠军后升入2017LPL夏季联赛。SNG是LPL舞台上的新生力量，其敢打敢拼的风格让其迅速崭露头角",
+		    	]
+	    },
+	    {
+	    	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/logo/rng.png",
+	    	title:"关于RNG",
+	    	content:[
+		    	"皇族电子竞技俱乐部（RNG）成立于2012年5月，现拥有英雄联盟、守望先锋、王者荣耀和街头篮球4个游戏分部。其中皇族英雄联盟分部取得2013年、2014年英雄联盟全球总决赛亚军，2016年LPL春季赛冠军，2017年德玛西亚杯长沙站冠军，2018年LPL春季赛冠军、MSI季中邀请赛冠军及德玛西亚杯夏季赛冠军。",
+		    	"2018年RNG电竞高校伙伴正式启动，南大电竞社成为第一批合作伙伴，共同促进电竞事业在国内的良性健康发展，让更多的人能够感受并理解电竞文化！"
+		    	]
+	    },
+	    ]
     }
+  },
+  components:{
+  	PartnerCard,
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.partner{
+	margin-bottom: 100px;
+}
 </style>
