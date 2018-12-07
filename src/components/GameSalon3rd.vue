be6fe817af25c4c57ba8b01cadece267d147ec9e
Yuxin Wu <876465382@qq.com>
2018-12-07 14:58:50 +0800

    临时保存

--- src/components/GameSalon3rd.vue
+++ src/components/GameSalon3rd.vue
@@ -5,28 +5,27 @@
   		<img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part1.png" class="pic">
   		<div class="body">
   			<div class="part2">
-  				<div class="col-1">
+  				<div class="video1"><video src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/video/SpiderMan.mp4" width="1200px" height="700px" muted autoplay loop></video></div>
+  				<div class="video-logos">
+  					<a href="https://www.sonkwo.com/products/2293?game_id=2293&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/overcooked_logo.png"></a>
+  					<a href="http://act.wangyuan.com/gjqt3/201811/sale/index.html" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/gujian3_logo.png"></a>
+  					<a href="https://store.playstation.com/zh-hans-hk/product/UP1004-CUSA03041_00-ASIAPLACEHOLDER0" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/rdr2_logo.png"></a>
+  					<a href="https://store.steampowered.com/app/857950/Incredible_Mandy/" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/mandy_logo.png"></a>
+  				</div>
+  			</div>
+		    <div class="part3">
+		    	<div class="col-1">
   					2018年12月16日 </br>
   					上午场 09:00-12:00 </br>
   					下午场 13:00-17:00 </br>
   					江苏省南京市栖霞区仙林大道163号 </br>
   					南京大学仙林校区九食堂西区 </br>
   					</br>
+  				</div>
+  				<div class="col-2">				
   					<div class="baidumap" id="allmap"></div>
   				</div>
-  				<div class="col-2">
-  					NJU电玩姬是南大电竞社电玩分部运营的 </br>
-  					公众号，除了发布每次活动的信息， </br>
-  					还会推送STEAM/PS4/X1等平台的 </br>
-  					新游资讯、游戏评测、折扣优惠等等。 </br>
-  					</br>
-  					目前已覆盖QQ、微信、微博三平台， </br>
-  					推荐关注QQ公众号，与电玩姬直接互动！ </br>
-  				</div>
-  				
-  			</div>
-		    <div class="part3">
-		    	<div class="p3-frame">
+		    	<!-- <div class="p3-frame">
 		    		<a href="https://www.sonkwo.com/products/2293?game_id=2293&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/overcooked_logo.png"></a>
 		    		<a href="https://www.sonkwo.com/products/3593?game_id=3658&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/overcooked2_logo.png"></a>    	
 			    	<a href="https://store.steampowered.com/app/857950/Incredible_Mandy/" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/mandy_logo.png"></a>
@@ -38,10 +37,23 @@
 			    	<div class="black-block"></div>
 			    	<a href="https://www.ubisoft.com.cn/games/console/jd2019" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/jd19_logo.png"></a>
 			    	<a href="http://www.sonkwo.hk/products/5?game_id=7&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/re2_logo.png"></a>
-		    	</div>
+		    	</div> -->
 		    	
 		    </div>
 		    <div class="part4">
+		    	<div class="p4-frame">
+			    	
+		    		<a href="https://www.sonkwo.com/products/3593?game_id=3658&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/overcooked2_logo.png"></a>
+		    		<a href="https://store.steampowered.com/app/698870/Metaverse_Keeper/" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/leak_logo.png"></a>
+		    		<a href="https://store.playstation.com/zh-hans-hk/product/HP9000-CUSA09893_00-ASIAPLACEHOLDER0" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/msm_logo.png"></a>
+		    		<a href="https://www.sonkwo.com/products/3796?game_id=3907&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/sc6_logo.png"></a>
+					<div class="black-block"></div>
+			    	<a href="https://www.ubisoft.com.cn/games/console/jd2019" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/jd19_logo.png"></a>
+			    	<a href="http://www.sonkwo.hk/products/5?game_id=7&rcode=cbfcf0c1c4" target="_blank"><img src="https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/re2_logo.png"></a>
+			    	
+		    	</div>
+		    </div>
+		    <div class="part5">
 		    	<div class="stage">
 		    		《灵魂能力6》晋级赛 </br>
 		    		</br>
@@ -50,7 +62,7 @@
 		    		规则待定 </br>
 		    	</div>
 		    </div>
-		    <div class="part5">
+		    <div class="part6">
 		    	<div class="stage">
 		    		《舞力全开2019》比舞大会 </br>
 		    		</br>
@@ -59,7 +71,17 @@
 		    		规则待定 </br>
 		    	</div>
 		    </div>
-		    <div class="part6"></div>
+		    <div class="part7"></div>
+		    <div class="part8">
+		    	<div class="intro">
+		    		NJU电玩姬是南大电竞社电玩分部运营的</br>
+		    		公众号，除了发布每次活动的信息，</br>
+		    		还会推送STEAM/PS4/X1等平台的</br>
+		    		新游资讯、游戏评测、折扣优惠等等。</br>
+		    		目前已覆盖QQ、微信、微博三平台，</br>
+		    		推荐关注QQ公众号，与电玩姬直接互动！</br>
+		    	</div>
+		    </div>
   		</div>
 
   	</div>
@@ -96,7 +118,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .baidumap{
-	width: 520px;
+	width: 550px;
 	height: 220px;
 	margin-left: 14px;
 	float: left;
@@ -111,6 +133,7 @@ export default {
 .back{
 	background-color: #090917;
 	position: relative;
+	z-index: -2;
 }
 .body{
 	width: 1222px;
@@ -123,26 +146,48 @@ export default {
 }
 .part1{
 	/*width: 1920px;*/
+
 	width: 1920vw;
 	height: 471px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part1.png);
 }
 .part2{
-
-	width: 1222px;
-	height: 544px;
+	width: 1274px;
+	height: 736px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part2.png);
 }
+.video1{
+	position: absolute;
+	padding-top: 18px;
+	margin-left: 40px;
+	width: 1200px;
+	height: 600px;
+	z-index: -1;
+}
+.video-logos{
+	padding-top: 580px;
+	margin-left: 60px;
+	width: 1170px;
+	height: 109px;
+}
+.video-logos a{
+	display: block;
+	float: left;
+	margin:10px 45px;
+}
+.video-logos img{
+
+}
 .part3{
-	width: 1222px;
-	height: 485px;
+	width: 1274px;
+	height: 324px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part3.png);
 }
-.p3-frame{
+.p4-frame{
 	width: 1170px;
 	height: 350px;
 	padding-top: 50px;
-	padding-left: 30px;
+	padding-left: 50px;
 }
 .black-block{
 	width: 200px;
@@ -150,14 +195,14 @@ export default {
 	float: left;
 	margin:20px 45px;
 }
-.part3 img{
+.part4 img{
 	display: block;
 	float: left;
-	margin:20px 45px;
+	margin:10px 45px;
 }
 .part4{
-	width: 1222px;
-	height: 434px;
+	width: 1274px;
+	height: 325px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part4.png);
 }
 .stage{
@@ -167,19 +212,35 @@ export default {
 	padding-top: 140px;
 }
 .part5{
-	width: 1222px;
-	height: 431px;
+	width: 1274px;
+	height: 433px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part5.png);
 }
 .part6{
-	width: 1222px;
-	height: 653px;
+	width: 1274px;
+	height: 433px;
 	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part6.png);
 }
+.part7{
+	width: 1274px;
+	height: 557px;
+	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part7.png);
+}
+.part8{
+	width: 1274px;
+	height: 416px;
+	background: url(https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/GameSalon3rd/part8.png);
+}
+.intro{
+	width: 600px;
+	height: 380px;
+	margin-left: 640px;
+	padding-top: 70px;
+}
 .col-1{
-	width: 560px;
+	width: 510px;
 	height: 160px;
-	margin-left: 30px;
+	margin-left: 80px;
 	padding-top: 80px;
 	float: left;
 }
@@ -187,7 +248,7 @@ export default {
 	width: 560px;
 	height: 220px;
 	margin-left: 60px;
-	padding-top: 80px;
+	padding-top: 50px;
 	float: left;
 }
 </style>
