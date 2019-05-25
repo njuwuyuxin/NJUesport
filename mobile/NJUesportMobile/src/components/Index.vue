8c89ac56bd06b6b3fe3d06dcf4dd3ad0c93fb2e2
BIOKUBE <BIOKUBE@users.noreply.github.com>
2019-05-25 15:07:19 +0800

    Index.vue 2019.05.25 update#1

--- mobile/NJUesportMobile/src/components/Index.vue
+++ mobile/NJUesportMobile/src/components/Index.vue
@@ -25,29 +25,48 @@ export default {
     return {
       activities:[
       {
+      	title:"游戏沙龙第四期",
+      	date:"2019年6月待定",
+      	class:"游戏沙龙",
+      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salontemp.jpg",
+      	href:"",
+        id:"1"
+      },
+      {
+      	title:"2019体育月游园会",
+      	date:"2019年4月20日",
+      	class:"展台活动",
+      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2019tiyuyue.jpg",
+      	href:"https://mp.weixin.qq.com/s/pYl8c4WeJyhziek-NRCaDA",
+        id:"2"
+      },
+      {
+      	title:"2019社团巡礼",
+      	date:"2019年4月13日",
+      	class:"展台活动",
+      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2019shexun.jpg",
+      	href:"https://mp.weixin.qq.com/s/WmqvB3FJfOsWqmy7C9I_yg",
+        id:"3"
+      },
+      {
       	title:"游戏沙龙第三期",
       	date:"2018年12月16日",
       	class:"游戏沙龙",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salon20181216.jpg",
         href:"#",
         routerLink:"/GameSalon3rd",
-        id:"1"
+        id:"4"
       },
+      ],
+
+      reviews:[
       {
       	title:"2018冬至未至社团嘉年华",
       	date:"2018年12月8日",
       	class:"展台活动",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2018dongzhi.jpg",
       	href:"https://mp.weixin.qq.com/s/hWTouIDpLI6FKggTMi6kmA",
-        id:"2"
-      },
-      {
-      	title:"2018南京大学游戏大选",
-      	date:"2018年11月14日至25日",
-      	class:"游戏大选",
-      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2018njuga.jpg",
-      	href:"https://tp.wjx.top/jq/31130953.aspx",
-        id:"3"
+        id:"1"
       },
       {
       	title:"游戏沙龙万圣节特别期",
@@ -55,18 +74,15 @@ export default {
       	class:"游戏沙龙",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salon20181028.jpg",
       	href:"https://mp.weixin.qq.com/s/Qf57HVDVET9UCh5oHBXl5g",
-        id:"4"
+        id:"2"
       },
-      ],
-
-      reviews:[
       {
       	title:"首届“新生杯”电子竞技大赛",
       	date:"2018年10月13日至14日",
       	class:"大型比赛",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2018xinshengbei.jpg",
       	href:"https://mp.weixin.qq.com/s/_ARdMvYzXtIQJLOtJJdR2Q",
-        id:"1"
+        id:"3"
       },
       {
       	title:"游戏沙龙第二期",
@@ -74,7 +90,7 @@ export default {
       	class:"游戏沙龙",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salon20181013.jpg",
       	href:"https://steamcn.com/t429743-1-1",
-        id:"2"
+        id:"4"
       },
       {
       	title:"首届\"天命杯\"刺激战场交流赛",
@@ -82,7 +98,7 @@ export default {
       	class:"大型比赛",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2018pubg.jpg",
       	href:"https://mp.weixin.qq.com/s/IxfZS0eXrAbSegLDc-294g",
-        id:"3"
+        id:"5"
       },
       {
       	title:"游戏沙龙第一期",
@@ -90,22 +106,6 @@ export default {
       	class:"游戏沙龙",
       	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salon20180527.jpg",
       	href:"https://steamcn.com/t389636-1-1",
-        id:"4"
-      },
-      {
-      	title:"游戏沙龙第零期",
-      	date:"2018年5月20日",
-      	class:"游戏沙龙",
-      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/salon20180520.jpg",
-      	href:"https://mp.weixin.qq.com/s/qH0So5BDfXkuYBY1Bw-9MQ",
-        id:"5" 
-      },
-      {
-      	title:"2018社团巡礼校园电玩展",
-      	date:"2018年4月21日",
-      	class:"展台活动",
-      	coverPic:"https://image-1253276483.cos.ap-shanghai.myqcloud.com/image/card/2018shexun.jpg",
-      	href:"https://steamcn.com/t381795-1-1",
         id:"6"
       },
       ],
