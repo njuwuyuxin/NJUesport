16c22ff4d5930957c12766cc6caada262f498fb7
wuyuxin <876465382@qq.com>
2019-08-06 21:50:51 +0800

    feat:去掉合作伙伴标题

--- src/components/utils/PartnerCard.vue
+++ src/components/utils/PartnerCard.vue
@@ -2,7 +2,7 @@
 	<div class="frame">
 		<div class="partnerCard">
 		    <a v-bind:href="PartnerInfo.href" target="_blank"><img class="pic" v-bind:src="PartnerInfo.coverPic"></a>
-		    <h2 class="title">{{PartnerInfo.title}}</h2>
+		    <!--<h2 class="title">{{PartnerInfo.title}}</h2>-->
 		    <div class="paras" v-for="para in PartnerInfo.content">{{para}}</div>
 		</div>
 	</div>
